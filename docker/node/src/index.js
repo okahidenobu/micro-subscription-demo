b86ba44214245fad3557940aa63a6bc25c6a4b88
oka hidenobu <okahidenobu@gmail.com>
2020-06-16 10:31:34 +0900

    no message

--- docker/node/src/index.js
+++ docker/node/src/index.js
@@ -1,8 +1,7 @@
-const {ApolloServer, gql} = require("apollo-server-micro")
-const {PubSub} = require("graphql-subscriptions")
-const micro = require("micro")
-const http = require("http")
-const {User} = require("./models")
+const {createServer} = require('http')
+const {ApolloServer, gql} = require('apollo-server-express')
+const express = require('express')
+
 const {resolvers} = require('./resolvers')
 
 // Construct a schema, using GraphQL schema language
@@ -28,18 +27,23 @@ const typeDefs = gql`
     }
 `
 
+const PORT = 3000
+const app = express()
+
 const server = new ApolloServer({
   typeDefs,
   resolvers
 })
 
-const PORT = 3000
-const handler = server.createHandler()
-const httpServer = new http.Server(handler)
+server.applyMiddleware({app})
+
+const httpServer = createServer(app)
 server.installSubscriptionHandlers(httpServer)
 
+// ⚠️ Pay attention to the fact that we are calling `listen` on the http server variable, and not on `app`.
 httpServer.listen(PORT, () => {
+  // console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
+  // console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`)
   console.log(`🚀 Server ready at http://localhost:8092`)
   console.log(`🚀 Subscriptions ready at ws://localhost:8092`)
 })
-
